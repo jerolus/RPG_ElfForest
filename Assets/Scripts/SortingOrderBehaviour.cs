e919e7870ede243e9a58c3efa3d45628250bfb06
Jesús María Parral Puebla <jesusmaria31@gmail.com>
2019-07-13 15:11:17 +0200

    Animation added. Destroyable wip.

---
+++ Assets/Scripts/SortingOrderBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortingOrderBehaviour : MonoBehaviour
+{
+	public bool fixInRuntime = false;
+
+	private SpriteRenderer m_sprite;
+
+    private void Start()
+    {
+		m_sprite = gameObject.GetComponent<SpriteRenderer>();
+		m_sprite.sortingLayerName = "Player";
+		m_sprite.sortingOrder = Mathf.RoundToInt(-transform.position.y * 100);
+    }
+
+    private void Update()
+    {
+        if(fixInRuntime)
+		{
+			m_sprite.sortingOrder = Mathf.RoundToInt(-transform.position.y * 100);
+		}
+    }
+}
