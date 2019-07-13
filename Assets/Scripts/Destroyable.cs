aa62aa7a455fce614516732fcc507e7f0b609fc5
Jesús María Parral Puebla <jesusmaria31@gmail.com>
2019-07-13 16:16:42 +0200

    Destroyable behaviour done. MainMenu behaviour done. PauseMenu behaviour done.

--- Assets/Scripts/Destroyable.cs
+++ Assets/Scripts/Destroyable.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class Destroyable : MonoBehaviour
 {
-    void Start()
+	public string destroyAnimationName;
+
+	private Animator m_animator;
+	private Collider2D m_collider;
+
+	private void Start()
     {
-        
+		m_animator = gameObject.GetComponent<Animator>();
+		m_collider = gameObject.GetComponent<Collider2D>();
+	}
+
+    private void Update()
+    {
+		AnimatorStateInfo animInfo = m_animator.GetCurrentAnimatorStateInfo(0);
+
+		if (animInfo.IsName(destroyAnimationName) && animInfo.normalizedTime >= 1)
+		{
+			Destroy(gameObject);
+		}
     }
 
-    void Update()
-    {
-        
-    }
+	private IEnumerator OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Attack")
+		{
+			m_animator.Play(destroyAnimationName);
+			yield return new WaitForSeconds(0.25f);
+			m_collider.enabled = false;
+		}
+	}
 }
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+	private static GameController m_instance;
+	public GameObject pauseMenu;
+
+	private bool isPlaying = false;
+
+	private void Awake()
+	{
+		SetInstance();
+		DontDestroyOnLoad(gameObject);
+	}
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+		if(Input.GetKeyDown(KeyCode.P) && isPlaying)
+		{
+			CheckPause();
+		}
+	}
+
+	public GameController GetInstance()
+	{
+		if (m_instance != null)
+		{
+			return m_instance;
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	private void SetInstance()
+	{
+		if (!m_instance)
+		{
+			m_instance = this;
+		}
+	}
+
+	public void StartGame()
+	{
+		isPlaying = true;
+		SceneManager.LoadScene("Prototype");
+	}
+
+	public void MainMenu()
+	{
+		isPlaying = false;
+		pauseMenu.SetActive(false);
+		SceneManager.LoadScene("Menu");
+	}
+
+	public void ResumeGame()
+	{
+		CheckPause();
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+
+	private void CheckPause()
+	{
+		pauseMenu.SetActive(!pauseMenu.activeSelf);
+		if (pauseMenu.activeSelf)
+		{
+			Time.timeScale = 0;
+		}
+		else
+		{
+			Time.timeScale = 1;
+		}
+	}
+}
